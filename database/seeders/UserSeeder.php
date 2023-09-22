9ce548ba738e3f8acecce64dcf8e9c6983b9ad99
ImadeddineElalouaji <131325828+ImadeddineElalouaji@users.noreply.github.com>
2023-09-22 01:13:16 +0100

    seeder
    ADMIN AND USER

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+// database/seeders/UserSeeder.php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    public function run()
+    {
+        // Insert admin user
+        DB::table('users')->insert([
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('admin1234'),
+            'role' => 3, // Assuming role 3 represents admin
+        ]);
+
+        // Insert utilisateur user
+        DB::table('users')->insert([
+            'name' => 'utilisateur',
+            'email' => 'user@gmail.com',
+            'password' => Hash::make('user1234'),
+            'role' => 2, // Assuming role 2 represents utilisateur
+        ]);
+    }
+}
